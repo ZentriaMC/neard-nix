7d52a4e73d1dde99cd4c8f5572b5e259b9a86e5f
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-08-16 15:51:56 +0300

    neard*: drop huge wall of text before doCheck
    When I ran tests manually in neard directory, it only ran very few tests related to arguments parsing and not related to protocol whatsoever

--- neard-shardnet.nix
+++ neard-shardnet.nix
@@ -57,21 +57,7 @@ rustPlatform.buildRustPackage rec {
   NEAR_RELEASE_BUILD = "release";
 
   cargoBuildFlags = lib.optional (features != [ ]) "--features=${lib.concatStringsSep "," features}";
-
-  # WARNING 2021-05-16: takes ram massively, >14GiB for purely linking (debug build)!
-  # NOTE 2021-07-22: vendoring seems to be broken
-  #    error: failed to run custom build command for `near-test-contracts v0.0.0 (/build/source/runtime/near-test-contracts)`
-  #
-  #    Caused by:
-  #      process didn't exit successfully: `/build/source/target/release/build/near-test-contracts-e7d9e8d0fe5c3dd3/build-script-build` (exit status: 1)
-  #      --- stderr
-  #      error: failed to select a version for the requirement `serde_json = "=1.0.62"`
-  #      candidate versions found which didn't match: 1.0.63
-  #      location searched: directory source `/build/neard-1.19.2-vendor.tar.gz` (which is replacing registry `https://github.com/rust-lang/crates.io-index`)
-  #      required by package `test-contract-rs v0.1.0 (/build/source/runtime/near-test-contracts/test-contract-rs)`
-  #      perhaps a crate was updated and forgotten to be re-vendored?
-  #      command `"cargo" "build" "--target=wasm32-unknown-unknown" "--release"` exited with non-zero status: ExitStatus(ExitStatus(25856))
-  doCheck = false;
+  doCheck = false; # tests only few CLI things
 
   meta = with lib; {
     platforms = platforms.linux ++ platforms.darwin;
