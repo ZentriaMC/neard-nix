b80bd69f60eb6ebc5beea5a8280a18dc917157f2
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-03-29 21:37:11 +0300

    neard*: bump rust to 1.68.0

--- flake.nix
+++ flake.nix
@@ -46,12 +46,12 @@
       in
       rec {
         packages.neard = pkgs.callPackage ./neard.nix {
-          rustPlatform = mkRustPlatform "stable" "1.67.1";
+          rustPlatform = mkRustPlatform "stable" "1.68.0";
           inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation DiskArbitration Foundation IOKit Security;
         };
 
         packages.neard-rc = pkgs.callPackage ./neard-rc.nix {
-          rustPlatform = mkRustPlatform "stable" "1.67.1";
+          rustPlatform = mkRustPlatform "stable" "1.68.0";
           inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation DiskArbitration Foundation IOKit Security;
         };
 
