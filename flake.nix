8ef6efc8d55ac2ac947a84bb2a3b5ccb3f4c83fa
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-05-25 13:28:11 +0300

    Add image revision label

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
               };
               Labels = {
                 "org.opencontainers.image.source" = "https://github.com/ZentriaMC/neard-nix";
+                "org.opencontainers.image.revision" = self.rev or "dirty";
               };
               Entrypoint = [ "/usr/bin/dumb-init" "--" ];
               Cmd = [ "neard" "--home" "/data" "--help" ];
