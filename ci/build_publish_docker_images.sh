8a292e4c59d7a19fd8219e141f9877c37744f60b
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-07-03 14:35:41 +0300

    Implement image signing

--- ci/build_publish_docker_images.sh
+++ ci/build_publish_docker_images.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 repo="${DOCKER_REPOSITORY:-docker.io/zentria/neard-nix}"
 current_system="$(nix-instantiate --eval -E --json 'builtins.currentSystem' | jq -r '.')"
 system="${current_system/-darwin/-linux}"
+sign_images=0
 
 nix_flags=(
         --extra-experimental-features "nix-command flakes"
@@ -17,6 +18,16 @@ nix_flags=(
 skopeo_flags=(
 	--insecure-policy
 )
+
+if [ -n "${COSIGN_KEY+x}" ] && [ "${COSIGN_SKIP:-false}" = "false" ]; then
+	# we usually have password for cosign key as well, ensure that it's around
+	: "${COSIGN_PASSWORD+x}"
+
+	sum="$(COSIGN_PASSWORD="$(echo -n "${COSIGN_PASSWORD}" | base64 -d)" cosign public-key --key <(echo -n "${COSIGN_KEY}" | base64 -d) | sha256sum - | cut -d' ' -f1)"
+	echo "Signing images with key '${sum}' (sha256)"
+
+	sign_images=1
+fi
 
 if ! [ "${current_system}" = "${system}" ]; then
 	echo ">>> Building for '${system}' on '${current_system}'"
@@ -33,5 +44,21 @@ drvs=(
 for drv in "${drvs[@]}"; do
 	tag="$(nix-instantiate --argstr system "${system}" --argstr drv "${drv}" --eval --json -E '{ system, drv }: let flake = builtins.getFlake (toString ./.); in flake.packages.${system}.${drv}.imageTag' | jq -r '.')"
 	tarball="$(nix "${nix_flags[@]}" build -L --json "${root}#${drv}" | jq -r '.[] | .outputs.out')"
-	skopeo "${skopeo_flags[@]}" copy docker-archive:"${tarball}" "docker://${repo}:${tag}"
+
+	digest_file="./digest.txt"
+	skopeo_image_args=(
+		--digestfile "${digest_file}"
+		docker-archive:"${tarball}"
+		"docker://${repo}:${tag}"
+	)
+
+	skopeo "${skopeo_flags[@]}" copy "${skopeo_image_args[@]}"
+
+	if (( sign_images )); then
+		COSIGN_PASSWORD="$(echo -n "${COSIGN_PASSWORD}" | base64 -d)" cosign sign \
+			--key <(echo -n "${COSIGN_KEY}" | base64 -d) \
+			--tlog-upload=false \
+			"${annotations[@]}" \
+			"${repo}@$(< "${digest_file}")"
+	fi
 done
