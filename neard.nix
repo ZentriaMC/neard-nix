7e6ee89830f24b510f6578bb34de586c2e1dd973
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-12-18 23:52:49 +0200

    neard: 1.36.1 -> 1.36.2

--- neard.nix
+++ neard.nix
@@ -16,9 +16,9 @@
 , Security
 , features ? [ ]
 }:
-rustPlatform.buildRustPackage rec {
+stdenv.mkDerivation rec {
   pname = "neard";
-  version = "1.36.1";
+  version = "1.36.2";
 
   buildInputs = [
     llvm
@@ -31,7 +31,7 @@ rustPlatform.buildRustPackage rec {
     IOKit
     Security
   ];
-  nativeBuildInputs = [ clang llvm.out perl pkg-config protobuf ];
+  nativeBuildInputs = [ rustPlatform.cargoSetupHook clang llvm.out perl pkg-config protobuf ];
 
   OPENSSL_NO_VENDOR = 1; # we want to link to OpenSSL provided by Nix
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
@@ -41,11 +41,17 @@ rustPlatform.buildRustPackage rec {
     owner = "near";
     repo = "nearcore";
     rev = "refs/tags/${version}";
-    hash = "sha256-SNR7WgnUnzPLMYKtcda1fZQuj3ETSjhJMrbygThlZ0o=";
+    hash = "sha256-7jP6NomoAxFoEszCXZsKHeFQmygA7Yx6DFxma/ZoZJM=";
   };
 
-  cargoPatches = [ ./patches/1.36.1/0001-Make-near-test-contracts-optional.patch ];
-  cargoHash = "sha256-8D1gLl/rPm0piCnUqLfzR21Rr76mQJptkcrKQo8CEQ4=";
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./patches/1.36.2/Cargo.lock;
+    outputHashes = {
+      "protobuf-3.0.2" = "sha256-HVNlMXZRNa9F8hr6sj75uuCvppR6mVOSumSLnye/F3Y=";
+    };
+  };
+
+  patches = [ ./patches/1.36.2/0001-Make-near-test-contracts-optional.patch ];
 
   postPatch = ''
     substituteInPlace neard/build.rs \
@@ -56,6 +62,14 @@ rustPlatform.buildRustPackage rec {
   CARGO_PROFILE_RELEASE_LTO = "thin";
   RUSTFLAGS = "-D warnings";
   NEAR_RELEASE_BUILD = "release";
+
+  buildPhase = ''
+    runHook preBuild
+
+    cargo build -p $buildAndTestSubdir --release $cargoBuildFlags
+
+    runHook postBuild
+  '';
 
   cargoBuildFlags = lib.optional (features != [ ]) "--features=${lib.concatStringsSep "," features}";
   doCheck = false; # tests only few CLI things
